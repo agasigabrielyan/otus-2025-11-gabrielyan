e1f1a4b69e741582070f01e53c2982aef025f74a
agasi79@list.ru <agasi79@list.ru>
2025-12-14 19:52:59 +0500

    chore(Bitrix24 - project): установлен composer

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php require ($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+use Symfony\Component\VarDumper\VarDumper;
+VarDumper::dump($_SERVER);
+?>
+
+<?php require ($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
