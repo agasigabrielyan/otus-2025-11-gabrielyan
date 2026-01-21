25ea4166593f8277b238fcfaf754c7ad355d79f6
agasi <agasi79@list.ru>
2026-01-21 20:29:19 +0500

    Feat(otus-project) Добавить в меню и ссылки

--- homework4/index.php
+++ homework4/index.php
@@ -39,6 +39,26 @@ while ($product = $res->fetch()) {
 
 // Вывод
 ?>
+<p>
+    <div>
+        <div class="mb-3">Репозиторий: <a target="_blank" href="https://github.com/agasigabrielyan/otus-2025-11-gabrielyan">https://github.com/agasigabrielyan/otus-2025-11-gabrielyan</a></div>
+        <div class="mb-3">ORM класс таблицы products :
+            <a target="_blank" href="/bitrix/admin/fileman_file_edit.php?path=%2Flocal%2FOtusApp%2FOtusDzfour%2FProductsTable.php&full_src=Y&site=s1&lang=ru&&filter=Y&set_filter=Y">
+                /bitrix/admin/fileman_file_edit.php?path=%2Flocal%2FOtusApp%2FOtusDzfour%2FProductsTable.php&full_src=Y&site=s1&lang=ru&&filter=Y&set_filter=Y
+            </a>
+        </div>
+        <div class="mb-3">Инфоблок категории :
+            <a target="_blank" href="/bitrix/admin/iblock_list_admin.php?IBLOCK_ID=20&type=lists&lang=ru&find_section_section=0&SECTION_ID=0&apply_filter=Y">
+                /bitrix/admin/iblock_list_admin.php?IBLOCK_ID=20&type=lists&lang=ru&find_section_section=0&SECTION_ID=0&apply_filter=Y
+            </a>
+        </div>
+        <div class="mb-3">Инфоблок производители :
+            <a target="_blank" href="/bitrix/admin/iblock_list_admin.php?IBLOCK_ID=21&type=lists&lang=ru&find_section_section=0&SECTION_ID=0&apply_filter=Y">
+                /bitrix/admin/iblock_list_admin.php?IBLOCK_ID=21&type=lists&lang=ru&find_section_section=0&SECTION_ID=0&apply_filter=Y
+            </a>
+        </div>
+    </div>
+</p>
     <h2>Список продуктов</h2>
     <table border="1" cellpadding="5" cellspacing="0">
         <tr>
