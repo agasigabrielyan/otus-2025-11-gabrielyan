c16383e98c48379cf6d36a4faff0e988f27e26f4
agasi <agasi79@list.ru>
2026-06-22 13:23:28 +0500

    Удалил папку local для следующего занятия с чистого листа

--- local/marketapp/config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    // Скопируйте в config.php и подставьте значения из vendors.bitrix24.ru
-    'client_id' => 'PASTE_CLIENT_ID',
-    'client_secret' => 'PASTE_CLIENT_SECRET',
-];
--- local/marketbot/config.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'client_id' => 'PASTE_CLIENT_ID',
-    'client_secret' => 'PASTE_CLIENT_SECRET',
-    'bot_code' => 'otus_crm_stats_bot',
-    'bot_name' => 'CRM-статистика otusgabrielyan',
-    'handler_url' => 'https://otusgabrielyan.ru/local/marketbot/handler.php',
-];
